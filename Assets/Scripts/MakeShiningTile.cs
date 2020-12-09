8a9c94197d260b01e8b9e6bdab23fa7a14e1b9ac
Aducj1910 <adish1910@gmail.com>
2020-12-09 17:27:58 +0530

    Added algorithm for rendering tiles

---
+++ Assets/Scripts/MakeShiningTile.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MakeShiningTile : MonoBehaviour
+{
+    public Tile shiningTile;
+    public Tilemap TilemapBackground;
+    public Tilemap TilemapShining;
+    public int width;
+    public int height;
+
+    void Start()
+    {
+        for(int w = 0; w < width; w++)
+        {
+            for(int h = 0; h < height; h++)
+            {
+                float wid = w + 0.5f;
+                float hei = h + 0.5f;
+
+                Vector3Int myPos = new Vector3Int(w, h, 0);
+                if(TilemapBackground.GetTile(myPos) != null)
+                {
+                    int passerNumber = Random.Range(1, 21);
+                    int dividedNumber = passerNumber / 20;
+                    if(dividedNumber == 1)
+                    {
+                        TilemapShining.SetTile(myPos, shiningTile);
+                    }
+                }
+            }
+        }
+    }
+}
+
